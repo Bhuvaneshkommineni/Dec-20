2be78b0da5c4d652bcc5926594128f4e283863be
Bhuvaneshkommineni <komminenibhuvanesh@gmail.com>
2024-12-20 12:10:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from'./components/NavBar'
+import Home from'./components/Home'
+import Movies from './components/movies'
+import Products from './components/Products'
+import EmpSal from './components/Empsal'
+import EmpApi from './components/EmpApi'
+export default function App() {
+  return (
+    <div>
+      
+      <EmpApi/>
+      </div>
+  )
+}
